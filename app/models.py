8a1b5c4b8ecb289221174bc48be0b462188a683b
timebigot <timebigot@gmail.com>
2016-04-28 16:36:34 -0400

    added is_lunch boolean and how_spicy to db

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class Category(models.Model):
     cat_name = models.CharField(max_length=30)
@@ -6,13 +7,16 @@ class Category(models.Model):
     def __str__(self):
         return self.cat_name
 
+menu_path = settings.STATIC_PATH + '/img/menu'
 class Menu(models.Model):
     item_name = models.CharField(max_length=30)
     detail = models.CharField(max_length=100, blank=True)
-    image = models.CharField(max_length=200)
-    price = models.CharField(max_length=10)
+    image = models.CharField(max_length=50)
+    price = models.CharField(max_length=10, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     in_season = models.BooleanField(default=1)
+    is_lunch = models.BooleanField(default=0)
+    how_spicy = models.PositiveSmallIntegerField(default=0)
 
     def __str__(self):
         return self.item_name
