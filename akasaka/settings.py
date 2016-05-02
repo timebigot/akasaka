2d2911db3ef7dbe1dda0fff9c59b0b6489f97f41
timebigot <timebigot@gmail.com>
2016-05-02 18:43:07 -0400

    staticfile dir var

--- akasaka/settings.py
+++ akasaka/settings.py
@@ -127,6 +127,6 @@ STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    STATIC_PATH,
     '/var/www/static/',
 ]
