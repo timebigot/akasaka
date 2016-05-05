316f64fc04642119e5f309ed0ed4d0ff5294ebf4
hshinf <hshinf@gmail.com>
2016-05-05 00:57:16 -0400

    fixed swipe

---
+++ static/js/swipe.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  
+$(".carousel").swiperight(function() {
+    $(this).carousel('prev');
+});
+$(".carousel").swipeleft(function() {  
+    $(this).carousel('next');
+});
+ 
+}); /* END document ready */
+ 
