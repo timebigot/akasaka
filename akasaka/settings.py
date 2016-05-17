0e28864faad69c69495acf150cc76cad0d753f57
hshinf <hshinf@gmail.com>
2016-05-17 17:34:39 -0400

    added few img and setting debug

--- akasaka/settings.py
+++ akasaka/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ag)9q%b)n^-kan4&jusfbk7^5j*tj6op1o=%fdve=6t9o7jin3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
